238c9055e9b2adda1e357e67b1b20ed320c51662
ipekozyonum <iozyonum@etu.edu.tr>
2021-02-24 21:57:42 +0300

    implementing java and xml files for essential activities

--- HowToSurvive/app/src/main/java/com/example/howtosurvive/Kullanici_Girisi_Activity.java
+++ HowToSurvive/app/src/main/java/com/example/howtosurvive/Kullanici_Girisi_Activity.java
@@ -7,61 +7,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Kullanici_Girisi_Activity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide(); //default action barı kaldır
 
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); //default action barı kaldır
         drawerLayout = findViewById(R.id.drawer_layout);
 
-        Button dogal_afet_sayfasi = (Button) findViewById(R.id.dogal_afet_but);
-        dogal_afet_sayfasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dogal_afet_sayfasina_gec(); // dogal afet butonuna tıklayınca bu metodu cagır
-            }
-        });
-
-        Button acil_durum_sayfasi = (Button) findViewById(R.id.acil_durum_but);
-        acil_durum_sayfasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                acil_durum_sayfasina_gec(); // acil durum butonuna tıklayınca bu metodu cagır
-            }
-        });
-
-        Button blog_sayfasi = (Button) findViewById(R.id.blog_but);
-        blog_sayfasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                blog_sayfasina_gec(); // blog butonuna tıklayınca bu metodu cagır
-            }
-        });
-
-        Button iletisim_sayfasi = (Button) findViewById(R.id.iletisim_but);
-        iletisim_sayfasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                iletisim_sayfasina_gec(); // iletişim butonuna tıklayınca bu metodu cagır
-            }
-        });
-
-        Button kullanici_girisi_sayfasi = (Button) findViewById(R.id.kullanici_girisi_but);
-        kullanici_girisi_sayfasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                kullanici_girisi_sayfasina_gec();
-            }
-        });
-
+        setContentView(R.layout.activity_kullanici_girisi);
     }
 
     public void ClickMenu(View view){
@@ -73,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ClickAnasayfa(View view){
-        recreate();
+        anasayfaya_gec();
     }
 
     public void ClickDogalAfet(View view){
@@ -93,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ClickKullaniciGiris(View view){
-        kullanici_girisi_sayfasina_gec();
+        recreate();
     }
 
     public void dogal_afet_sayfasina_gec(){
@@ -119,6 +77,11 @@ public class MainActivity extends AppCompatActivity {
     public void kullanici_girisi_sayfasina_gec(){
         Intent intent_kullanici = new Intent(this,Kullanici_Girisi_Activity.class);
         startActivity(intent_kullanici);
+    }
+
+    public void anasayfaya_gec(){
+        Intent intent_anasayfa = new Intent(this,MainActivity.class);
+        startActivity(intent_anasayfa);
     }
 
 }
