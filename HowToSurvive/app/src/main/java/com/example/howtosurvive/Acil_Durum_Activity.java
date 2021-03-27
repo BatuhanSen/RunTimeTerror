eaa1967ba99b7aba0c15e85b9edf0d00f760d93e
ipekozyonum <iozyonum@etu.edu.tr>
2021-03-27 16:40:22 +0300

    Front-end for acil_durum activity and get current location when pressed emergency button

--- HowToSurvive/app/src/main/java/com/example/howtosurvive/Acil_Durum_Activity.java
+++ HowToSurvive/app/src/main/java/com/example/howtosurvive/Acil_Durum_Activity.java
@@ -1,18 +1,40 @@
 package com.example.howtosurvive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Acil_Durum_Activity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class Acil_Durum_Activity extends AppCompatActivity implements LocationListener {
     DrawerLayout drawerLayout;
-    String username_res,id_res;
-    String name_res,mail_res,gender_res;
+    String username_res, id_res;
+    String name_res, mail_res, gender_res;
+    EditText ekstra_ihtiyac, kac_kisi;
+    Button acil_but;
+
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +50,64 @@ public class Acil_Durum_Activity extends AppCompatActivity {
         mail_res = intent.getStringExtra("mail");
         name_res = intent.getStringExtra("name");
         gender_res = intent.getStringExtra("gender");
+
+        ekstra_ihtiyac = findViewById(R.id.ekstra_ihtiyac);
+        kac_kisi = findViewById(R.id.kac_kisi);
+        acil_but = findViewById(R.id.acil_but);
+
+        //uygulamanın konum kullanmasina izin yoksa ekrana izin isteme cikar
+        if (ContextCompat.checkSelfPermission(Acil_Durum_Activity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Acil_Durum_Activity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+
+        acil_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adresBul();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    public void adresBul() {
+
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }*/
+        try {
+
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, Acil_Durum_Activity.this);
+        }catch (Exception e){
+            e.getStackTrace();
+        };
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        //latitude ve longitude ekranda gosterme
+        Toast.makeText(this,""+location.getLatitude()+","+location.getLongitude(),Toast.LENGTH_SHORT).show();
+
+        try {
+            Geocoder geocoder = new Geocoder(Acil_Durum_Activity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            String adres = addresses.get(0).getAddressLine(0);
+            //adresi ekranda gosterme
+            Toast.makeText(Acil_Durum_Activity.this,"Konumunuz alındı.\n"+adres,Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void ClickMenu(View view){
