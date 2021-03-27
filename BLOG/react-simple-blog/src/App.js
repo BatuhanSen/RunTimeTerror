1fa9e4ab86fd44b9ce26ab8ece610078bf732a78
yykyunuss <yunusyyk33@gmail.com>
2021-03-27 14:25:28 +0300

    Web Blog Page

---
+++ BLOG/react-simple-blog/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DisplayAllPosts from './Components/DisplayAllPosts';
+
+const App = ( ) => {
+  return (
+    <>
+      <DisplayAllPosts />
+    </>
+  );
+};
+export default App;
