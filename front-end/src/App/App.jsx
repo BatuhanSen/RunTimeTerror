16e4431f8a711e524a029f9c77a4cc90b2235fbf
BatuhanSen <55840322+BatuhanSen@users.noreply.github.com>
2021-04-14 20:17:42 +0300

    New routes added.
    New routes added in App.js

--- front-end/src/App/App.jsx
+++ front-end/src/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
@@ -8,6 +9,11 @@ import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
+import ContactPage from '../ContactPage/ContactPage';
+import EmergencyPage from '../EmergencyPage/EmergencyPage';
+import UserPage from '../UserPage/UserPage';
+
+
 
 class App extends React.Component {
     constructor(props) {
@@ -17,11 +23,15 @@ class App extends React.Component {
             // clear alert on location change
             this.props.clearAlerts();
         });
+
     }
+
+    
 
     render() {
         const { alert } = this.props;
         return (
+            
             <div className="jumbotron">
                 <div className="container">
                     <div className="col-sm-8 col-sm-offset-2">
@@ -33,9 +43,13 @@ class App extends React.Component {
                                 <PrivateRoute exact path="/" component={HomePage} />
                                 <Route path="/login" component={LoginPage} />
                                 <Route path="/register" component={RegisterPage} />
+                                <Route path="/contact" component={ContactPage} />
+                                <Route path="/emergency" component={EmergencyPage} />
+                                <Route path="/user" component={UserPage} />
                                 <Redirect from="*" to="/" />
                             </Switch>
                         </Router>
+                        
                     </div>
                 </div>
             </div>
