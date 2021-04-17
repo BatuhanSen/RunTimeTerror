dd1850e7fd8bb551d55571daed3c8591afebdae1
yykyunuss <yunusyyk33@gmail.com>
2021-04-18 00:36:56 +0300

    Google-Facebook Authentication and Blog Page merged

--- BLOG/react-simple-blog/src/Components/CreateNewPost.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useRef, useEffect } from "react";
-import { storage } from "../firebase";
-
-const CreateNewPost = props => {
-  return (
-    <div></div>
-  );
-};
-export default CreateNewPost;
