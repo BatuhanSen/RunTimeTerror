3104cdae952b67c954473b913988eabc40b06869
BatuhanSen <55840322+BatuhanSen@users.noreply.github.com>
2021-04-17 23:23:50 +0300

    Get Location for users
    NewLocation page fixed.

--- front-end/src/NewLocation/NewLocation.js
+++ front-end/src/NewLocation/NewLocation.js
@@ -1,37 +1,249 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../store/actions/index";
 
 
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+  } from "@react-google-maps/api";
+  import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+  } from "use-places-autocomplete";
+  import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+  } from "@reach/combobox";
+  import { formatRelative } from "date-fns";
+  
+  import "@reach/combobox/styles.css";
+  import mapStyles from "./mapStyles";
+  
+  const libraries = ["places"];
+  const mapContainerStyle = {
+    height: "50vh",
+    width: "50vw",
+  };
+  const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+  };
+  const center = {
+    lat: 39.933365,
+    lng: 32.859741,
+  };
+  
 
-class NewLocation extends React.Component {
+  function NewLocation(props) {
+    const { isLoaded, loadError } = useLoadScript({
+      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+      libraries,
+    });
+    const [markers, setMarkers] = React.useState([]);
+    const [selected, setSelected] = React.useState(null);
+    const [state, setState] = React.useState(null);
+  
+    const onMapClick = React.useCallback((e) => {
+      console.log(e.latLng.lat(), e.latLng.lng());
+      setState(e.latLng);
+      setMarkers((current) => [
+        ...current,
+        {
+          lat: e.latLng.lat(),
+          lng: e.latLng.lng(),
+          time: new Date(),
+        },
+      ]);
+    }, []);
 
 
+  
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+      mapRef.current = map;
+    }, []);
+  
+    const panTo = React.useCallback(({ lat, lng }) => {
+      mapRef.current.panTo({ lat, lng });
+      mapRef.current.setZoom(14);
+    }, []);
+  
+  console.log(markers);
     
+    if (loadError) return "Error";
+    if (!isLoaded) return "Loading...";
+
+    /*const handleCity = (event) => {
+      this.setState({
+        ...this.state,
+        city: event.target.value,
+      });
+    };*/
+
+ /* const handleNewLoc = (e) => {
+      e.preventDefault();
+      this.props.newloc(this.user,this.state.city, this.state.latitude, this.state.longitude);
+    };*/
     
+  
+    return (
+      <div>
+        <h1>
+          Konumunuzu Haritadan Seçin{" "}
+          
+        </h1>
+  
+        <Locate panTo={panTo} />
+        <Search panTo={panTo} />
+  
+        <GoogleMap
+          id="map"
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+          options={options}
+          onClick={onMapClick}
+          onLoad={onMapLoad}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={`${marker.lat}-${marker.lng}`}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              onClick={() => {
+                setSelected(marker);
+              }}
+              icon={{
+                url: `../logo.svg`,
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(15, 15),
+                scaledSize: new window.google.maps.Size(30, 30),
+              }}
+            />
+          ))}
+  
+          {selected ? (
+            <InfoWindow
+              position={{ lat: selected.lat, lng: selected.lng }}
+              onCloseClick={() => {
+                setSelected(null);
+              }}
+            >
+              <div>
+                <h2>
+                  <span role="img" aria-label="survive">
+                    
+                  </span>{" "}
+                  Seçili Konum
+                </h2>
+                <p>Spotted {formatRelative(selected.time, new Date())}</p>
+              </div>
+            </InfoWindow>
+          ) : null}
+        </GoogleMap>
+        <br></br>
+        <br></br>
 
-    render() {
-        const { user, users } = this.props;
 
-        
-        return (
-
-            <div className="col-md-6 col-md-offset-3">
-                
-                
-                <div className="form-group">
-                    <label htmlFor="location">Konum adı</label>
-                    <input type="location" className="form-control" name="location" /> <br></br>          
-                    <button className="btn btn-primary">Yeni Konum oluştur</button>
-                </div>
-                 
+          <div className="field">
+              <label>Konum Adı</label><br ></br>
+              <input
+                type="text"
+                placeholder="Ornegin EV"
+               // onChange={this.handleCity}
+                required
+              />
             </div>
-            
-        );
-    }
-    
-}
 
+            <div className="field buttons"/*onSubmit={this.handleNewLoc}*/>
+              <button type="submit"onClick={() => (props.newloc(props.user, "ev", state.lat(), state.lng()))}> Konumu Kaydet </button>
+              <button onClick={() => (window.location = "/")}></button>
+            </div>
+      </div>
+    );
+  }
+  
+  function Locate({ panTo }) {
+    return (
+      <button
+        className="locate"
+        onClick={() => {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              panTo({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              });
+            },
+            () => null
+          );
+        }}
+      >
+        mecvut konum
+      </button>
+    );
+  }
+  
+  function Search({ panTo }) {
+    const {
+      ready,
+      value,
+      suggestions: { status, data },
+      setValue,
+      clearSuggestions,
+    } = usePlacesAutocomplete({
+      requestOptions: {
+        location: { lat: () => 43.6532, lng: () => -79.3832 },
+        radius: 100 * 1000,
+      },
+    });
+  
+    // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+  
+    const handleInput = (e) => {
+      setValue(e.target.value);
+    };
+  
+    const handleSelect = async (address) => {
+      setValue(address, false);
+      clearSuggestions();
+  
+      try {
+        const results = await getGeocode({ address });
+        const { lat, lng } = await getLatLng(results[0]);
+        panTo({ lat, lng });
+      } catch (error) {
+        console.log("😱 Error: ", error);
+      }
+    };
+  
+    return (
+      <div className="search">
+       
+      </div>
+    );
+  }
 
-
-export default NewLocation;
+  const mapDispatchToState = (state) => {
+    return {
+      user: state.auth.user,
+      error: state.auth.error,
+      location: state.location.location,
+    };
+  };
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      newloc: (user, city, latitude, longitude) => dispatch(actions.newloc(user, city, latitude, longitude)),
+      
+    };
+  };
+  
+  export default connect(mapDispatchToState, mapDispatchToProps)(NewLocation);
+  
